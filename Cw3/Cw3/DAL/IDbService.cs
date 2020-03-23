da8f36ec996bab7b913535e440d940fdaf515c83
s17064 <61733654+s17064@users.noreply.github.com>
2020-03-23 19:13:53 +0200

    Add files via upload

---
+++ Cw3/Cw3/DAL/IDbService.cs
@@ -0,0 +1,14 @@
+﻿using Cw3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw3.DAL
+{
+        public interface IDbService
+    {
+        IEnumerable<Student> GetStudents();
+    }
+}
+
